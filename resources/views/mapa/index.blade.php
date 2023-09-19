0d47a9f541c35e8bbf42ee650d306fc9d69e1185
JowlArx <uarcefjoel@gmail.com>
2023-09-18 21:51:22 -0300

    agregue lupita al lao del buscado

--- resources/views/mapa/index.blade.php
+++ resources/views/mapa/index.blade.php
@@ -23,24 +23,28 @@
     </nav>
     <main>
         <div class="search-bar">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-            <path d="M21 21l-6 -6" />
-        </svg>
-            <input type="text" class="search-input" placeholder="Buscar...">
-        </div>
+        
+       </div>
     </main>
 <!--PARTE DE JOEL-->
 
 
 
 <!--MAPA-->
-<div class="container-xl contenedor">
-        <div class="row buscador">
+<div class="container-xl-contenedor">
+    <div class="center-buscador">
+        <div class="row-buscador">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                 <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                 <path d="M21 21l-6 -6" />
+            </svg>
             <input id="buscador-text" type="text" class="buscador-text">
-            <button type="button" class="btn btn-primary btn-buscar" id="btn-buscar">Buscar</button>
+            <button type="button" class="btn btn-primary btn-buscar" id="btn-buscar">
+                Buscar
+            </button>
         </div>
+    </div>    
         <div class="row contenedor-mapa-result">
             <ul id="ubicaciones" class="col-3 list-group"></ul>
             <div class="col-9">
@@ -50,8 +54,10 @@
     </div>
 </div>
 <!--MAPA-->
-
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-<script src="{{ asset('/js/mapa.js') }}"></script>
+<Div class="script-mapa">
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script src="{{ asset('/js/mapa.js') }}"></script>
+</Div>
+    
 </body>
 </html>
