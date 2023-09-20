35763e9d31c5e03e74cd6196b7594a762512aed2
JowlArx <uarcefjoel@gmail.com>
2023-09-20 19:00:40 -0300

    script menu hamburguesa (no anda)

--- resources/views/mapa/index.blade.php
+++ resources/views/mapa/index.blade.php
@@ -9,23 +9,39 @@
 <body>
 
 <!--PARTE DE JOEL-->
+ <!-- MENU HAMBURGUESA --> 
+<script>
+    const menuHamburguesa = document.querySelector(".menu-hamburguesa");
+    const menuHamburguesaDesplegable = document.querySelector(".menu-hamburguesa-desplegable");
 
-<header>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    menuHamburguesa.addEventListener("click", function() {
+      menuHamburguesaDesplegable.classList.toggle("mostrado");
+    });
+  </script>
+<header>    
+    <div class="menu-hamburguesa">
+  <a href="#" class="menu-hamburguesa-boton">
+    <i class="fas fa-bars"></i>
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M4 6l16 0" />
             <path d="M4 12l16 0" />
             <path d="M4 18l16 0" />
-        </svg>
-    </header>
-    <nav>
+    </svg>
+  </a>
+  <nav class="menu-hamburguesa-desplegable">
+    <ul>
+      <li><a href="#">Tema</a></li>
+      <li><a href="#">Albergues</a></li>
+      <li><a href="#">Acerca De</a></li>
+      <li><a href="#">Ayuda</a></li>
+      <li><a href="#">Salir</a></li>
+    </ul>
+  </nav>
+</div>
 
-    </nav>
-    <main>
-        <div class="search-bar">
-        
-       </div>
-    </main>
+    <!-- MENU HAMBURGUESA -->
+</header>
 <!--PARTE DE JOEL-->
 
 
